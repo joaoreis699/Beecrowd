ee3149d41394ddaad0c77dc730c3d9dc05185180
joaoreis699 <jp_reisamorim2000@hotmail.com>
2025-03-26 10:28:24 -0300

    another modification problem_1021.c

--- problem_1021.c
+++ problem_1021.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
-void notes(int *whole_part, int *arr, int size) {
+void notes(double *N, int *arr, int size) {
 
 	printf("NOTAS:\n");
 
 	for(int i = 0; i < size; i++) {
 	
-		int qnt = *whole_part / arr[i];
-		
-		*whole_part = (*whole_part % arr[i]);
+		int qnt = (int)(*N / arr[i]);
+	
+		*N -= qnt * arr[i];	
 
 		printf("%d nota(s) de R$ %d.00\n", qnt, arr[i]);
 	}
 }
 
-void coins(double *X, double *arr, int size) {
+void coins(double *N, double *arr, int size) {
 
 	printf("MOEDAS:\n");
 
-	int Y = (*X * 100);
 	int qnt = 0;
 
 	for(int i = 0; i < size; i++) {
 		
-		qnt = Y / (int)(arr[i] * 100);
-
-		Y = Y % (int)(arr[i] * 100);
+		qnt = (int)(*N / arr[i]);
+		
+		*N -= qnt * arr[i];	
 
 		printf("%d moeda(s) de R$ %.2lf\n", qnt, arr[i]);
 	}
@@ -39,17 +38,13 @@ int main() {
 
 	scanf("%lf", &N);
 
-	int whole_part = (int)(N);
-	
 	size = sizeof(arr_notes) / sizeof(arr_notes[0]);
 
-	notes(&whole_part, arr_notes, size);
-
-	double X = (N - (int)(N)) + whole_part;
+	notes(&N, arr_notes, size);
 
 	size = sizeof(arr_coins) / sizeof(arr_coins[0]);
 
-	coins(&X, arr_coins, size);
+	coins(&N, arr_coins, size);
 
 	return 0;
 }
